05854162cf9446a9a92da5bfd7035da875925a6a
Luandro <luandro@gmail.com>
2015-12-22 18:46:35 -0200

    converted to express

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,13 +1,5 @@
+// Server polyfill
 if(typeof require.ensure !== "function") require.ensure = function(d, c) { c(require) };
-
-// module.exports = {
-//   path: '/home',
-//   getComponent(location, cb) {
-//     require.ensure([], (require) => {
-//       cb(null, require('./containers/HomeContainer'))
-//     })
-//   }
-// }
 
 function loadData() {
 	console.log("Loading data for Home...")
--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
-import {Server} from "hapi";
-import h2o2 from "h2o2";
-import inert from "inert";
+/**
+ * TODO
+ * Break this file into smaller services:
+ * html render
+ * react render
+ * redux render
+ * falcor routes
+ */
+import express from "express";
+import favicon from "serve-favicon";
+import compression from 'compression';
+import path from 'path';
+import PrettyError from 'pretty-error';
+import parser from 'ua-parser-js';
+// React and React-Router
 import React from "react";
 import ReactDOM from "react-dom/server";
 import {RoutingContext, match} from "react-router";
 import createLocation from "history/lib/createLocation";
-import url from "url";
-import parser from 'ua-parser-js';
+// Redux
 import { Provider } from 'react-redux';
 import configureStore from "./models/store";
+// Radium
 import RadiumContainer from './routes/common/components/RadiumContainer';
+// Routes file
 import routes from "./routes";
 
 /**
@@ -18,115 +31,104 @@ import routes from "./routes";
 const store = configureStore();
 const initialState = store.getState();
 /**
- * Start Hapi server on port 8000.
+ * Start Express server on port 8000.
  */
+const pretty = new PrettyError();
+const app = express();
+const port = process.env.PORT || 8000;
 const hostname = process.env.HOSTNAME || "localhost";
-const server = new Server();
-
-server.connection({host: hostname, port: process.env.PORT || 8000});
-
-server.register(
-	[
-		h2o2,
-		inert,
-		// WebpackPlugin
-	],
-	(err) => {
-	if (err) {
-		throw err;
+app.listen(port, (error) => {
+	if (error) {
+		console.error(error);
+		return process.exit(3);
 	}
-
-	server.start(() => {
-		console.info("==> ✅  Server is listening");
-		console.info("==> 🌎  Go to " + server.info.uri.toLowerCase());
-	});
+	console.info("==> ✅  Server is listening");
+	console.info("==> 🌎  Go to http://localhost:%s.", port);
 });
 
 /**
  * Attempt to serve static requests from the public folder.
  */
-server.route({
-	method:  "GET",
-	path:    "/{params*}",
-	handler: {
-		file: (request) => "static" + request.path
-	}
-});
+const staticDir = path.join(__dirname,'..','static');
+app.use(compression());
+app.use(favicon(staticDir + '/favicon.ico'));
+app.use(express.static(staticDir));
+
 
 /**
  * Endpoint that proxies all GitHub API requests to https://api.github.com.
  */
-server.route({
-	method: "GET",
-	path: "/api/github/{path*}",
-	handler: {
-		proxy: {
-			passThrough: true,
-			mapUri (request, callback) {
-				callback(null, url.format({
-					protocol: "https",
-					host:     "api.github.com",
-					pathname: request.params.path,
-					query:    request.query
-				}));
-			},
-			onResponse (err, res, request, reply, settings, ttl) {
-				reply(res);
-			}
-		}
-	}
-});
+// server.route({
+// 	method: "GET",
+// 	path: "/api/github/{path*}",
+// 	handler: {
+// 		proxy: {
+// 			passThrough: true,
+// 			mapUri (request, callback) {
+// 				callback(null, url.format({
+// 					protocol: "https",
+// 					host:     "api.github.com",
+// 					pathname: request.params.path,
+// 					query:    request.query
+// 				}));
+// 			},
+// 			onResponse (err, res, request, reply, settings, ttl) {
+// 				reply(res);
+// 			}
+// 		}
+// 	}
+// });
 
 
 /**
  * Catch dynamic requests here to fire-up React Router.
  */
-server.ext("onPreResponse", (request, reply) => {
-	if (typeof request.response.statusCode !== "undefined") {
-    return reply.continue();
-  }
+app.get('*', (req, res, err) => {
+	if(err) {
+		console.log("err:", err)
+	}
+  	let location = createLocation(req.path);
 
-  let location = createLocation(request.path);
+  	match({routes, location: location}, (error, redirectLocation, renderProps) => {
+	    if (redirectLocation) {
+	      	res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
+	    }
+	    else if (error || !renderProps) {
+	    	console.error('ROUTER ERROR:', pretty.render(error));
+	    	res.status(500);
+	    }
+	    else {
+			const reactString = ReactDOM.renderToString(
+					<Provider store={store}>
+						<RadiumContainer radiumConfig={{userAgent: req.headers['user-agent']}}>
+							<RoutingContext {...renderProps} />
+						</RadiumContainer>
+					</Provider>
+			);
 
-  match({routes, location: location}, (error, redirectLocation, renderProps) => {
-    if (redirectLocation) {
-      reply.redirect(redirectLocation.pathname + redirectLocation.search)
-    }
-    else if (error || !renderProps) {
-      reply.continue();
-    }
-    else {
-		const reactString = ReactDOM.renderToString(
-				<Provider store={store}>
-					<RadiumContainer radiumConfig={{userAgent: request.headers['user-agent']}}>
-						<RoutingContext {...renderProps} />
-					</RadiumContainer>
-				</Provider>
-		);
+			const webserver = process.env.NODE_ENV === "production" ? "" : "//" + hostname + ":8080";
+			const ua = parser(req.headers['user-agent']);
 
-		const webserver = process.env.NODE_ENV === "production" ? "" : "//" + hostname + ":8080";
-		const ua = parser(request.headers['user-agent']);
-
-		let output = (
-			`<!doctype html>
-			<html lang="en-us">
-				<head>
-					<meta charset="utf-8">
-					<title>GuiaLa | Chapada dos Veadeiros</title>
-					<link rel="shortcut icon" href="/favicon.ico">
-				</head>
-				<body>
-					<div id="react-root">${reactString}</div>
-					<div id="tools"></div>
- 				<script>
- 					window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
- 					window.__UA__ = ${JSON.stringify(ua)}
- 				</script>
- 				<script src=${webserver}/dist/main.js></script>
- 			</body>
-			</html>`
- 		);
-    	reply(output);
-    }
+			let output = (
+				`<!doctype html>
+				<html lang="en-us">
+					<head>
+						<meta charset="utf-8">
+						<title>GuiaLa | Chapada dos Veadeiros</title>
+						<link rel="shortcut icon" href="/favicon.ico">
+					</head>
+					<body>
+						<div id="react-root">${reactString}</div>
+						<div id="tools"></div>
+	 				<script>
+	 					window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
+	 					window.__UA__ = ${JSON.stringify(ua)}
+	 				</script>
+	 				<script src=${webserver}/dist/main.js></script>
+	 			</body>
+				</html>`
+	 		);
+	    	res.status(200).send(output);
+	    }
   });
 });
